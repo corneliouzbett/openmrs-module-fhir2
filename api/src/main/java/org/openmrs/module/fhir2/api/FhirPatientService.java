8c1b4460db46d2a73bb34f7d9e953572c3f00c21
Ian <52504170+ibacher@users.noreply.github.com>
2019-12-16 08:39:01 -0500

    Add ability to search patient by name (#3)

--- api/src/main/java/org/openmrs/module/fhir2/api/FhirPatientService.java
+++ api/src/main/java/org/openmrs/module/fhir2/api/FhirPatientService.java
@@ -9,6 +9,8 @@
  */
 package org.openmrs.module.fhir2.api;
 
+import java.util.Collection;
+
 import org.hl7.fhir.r4.model.Identifier;
 import org.hl7.fhir.r4.model.Patient;
 import org.openmrs.PatientIdentifierType;
@@ -19,4 +21,9 @@ public interface FhirPatientService {
 	
 	PatientIdentifierType getPatientIdentifierTypeByIdentifier(Identifier identifier);
 	
+	Collection<Patient> findPatientByName(String name);
+	
+	Collection<Patient> findPatientByGivenName(String given);
+	
+	Collection<Patient> findPatientByFamilyName(String family);
 }
